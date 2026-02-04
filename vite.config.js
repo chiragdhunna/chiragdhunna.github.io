88a84360ddffab3f316158abaa43748e0819f1d1
chiragdhunna <chiragdhunna2468@gmail.com>
2026-02-04 21:40:38 +0530

    feat: add path aliases for improved module resolution in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -13,6 +14,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "pdfjs-dist": "pdfjs-dist/legacy/build/pdf",
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
+  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.svg", "**/*.gif"],
 });
